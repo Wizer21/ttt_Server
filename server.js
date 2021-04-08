0c94d4f534c884b3f6f24dba17c5ecca2b5846f5
Wizer <simon.tessier8@gmail.com>
2021-04-08 23:42:45 +0200

    Cross origin

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const http = require('http');
 const url = require('url')
 
-const hostname = '51.75.20.93';
+// const hostname = '51.75.20.93';
+const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  // Allow Cross origin requests
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+
   const url_parts = url.parse(req.url, true).pathname
   if(req.method === 'GET'){
-    
-    answer.end("done" + req)
+    res.end("done" + req)
   }
   else{    
     res.statusCode = 200;
