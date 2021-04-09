a899d9bd1bef57e96dffbe1152a718f2b1e9a126
Wizer <simon.tessier8@gmail.com>
2021-04-09 23:51:22 +0200

    Manage invite

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const hostname = '127.0.0.1'
 const port = 3000
 
 let userList = []
+let rooms = []
 
 function userListTimerControl(){
   let now = Date.now()
   for (let i = 0; i < userList.length; i++) {
     if (now - userList[i].lastRequestTime > 10000){
-      console.log("AFK", userList[i].name);
+      console.log("Kick ", userList[i].name)
       userList.splice(i, 1)
       i--
     }
   }
-  console.log("loop", userList);
   setTimeout(() => {
     userListTimerControl()
   }, 10000)
@@ -46,7 +46,6 @@ const server = http.createServer((req, res) => {
       // User connection
       const url_query = url.parse(req.url, true).query
       const username = url_query['username']
-      console.log("getUser(username)", getUser(username));
 
       if (getUser(username)){
         // User not avaible
@@ -57,10 +56,68 @@ const server = http.createServer((req, res) => {
         userList.push({
           name: username,
           lastRequestTime: Date.now(),
-          positions: []
+          positions: [],
+          mailBox: []
         })
-        console.log("userList", userList);
         res.end("true")
+      }
+    }
+    else if (url_parts == "/userlist"){
+      res.end(JSON.stringify(userList))
+    }
+    else if(url_parts == "/update"){      
+      const url_query = url.parse(req.url, true).query
+      const user = getUser(url_query['username'])
+      
+      if (user){
+        user.lastRequestTime = Date.now()
+        if (user.mailBox.length > 0){
+          res.end(JSON.stringify(user.mailBox))
+        }
+        else{
+          res.end("clean")
+        }
+      }
+    }
+    else if(url_parts == "/invite"){
+      const url_query = url.parse(req.url, true).query
+      const from = url_query['from']
+      const to = getUser(url_query['to'])
+      console.log("get invite");
+
+      if(to){
+        to.mailBox.push({
+          type: "invite",
+          from: from,
+          since: Date.now()
+        })
+        res.end("done")
+      }
+      else{
+        res.end("User not found")
+      }
+    }
+    else if (url_parts == "/open"){
+      const url_query = url.parse(req.url, true).query
+      const p1 = getUser(url_query['p1'])
+      const p2 = getUser(url_query['p2'])
+      
+      if (p1 || p2){
+        let id = rooms.length
+        rooms.push({
+          id: id,
+          p1: p1,
+          p1: p2
+        })
+  
+        p1.mailBox = [{
+          type: "game",
+          room: id
+        }]
+        p2.mailBox = [{
+          type: "game",
+          room: id
+        }]
       }
     }
   }
