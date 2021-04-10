232634dedd2072c5be01affb398e838c585b41a1
Wizer <simon.tessier8@gmail.com>
2021-04-10 18:31:23 +0200

    Game full playable

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ function getUser(username){
   return null
 }
 
+function getGame(gameId){
+  for (let game of rooms){
+    if (gameId == game.id){
+      return game
+    }
+  }
+}
+
 const server = http.createServer((req, res) => {
   // Allow Cross origin requests
 	res.setHeader('Access-Control-Allow-Origin', '*')
@@ -49,6 +57,7 @@ const server = http.createServer((req, res) => {
 
       if (getUser(username)){
         // User not avaible
+        res.writeHead(403)
         res.end("false")
       }
       else{
@@ -59,10 +68,12 @@ const server = http.createServer((req, res) => {
           positions: [],
           mailBox: []
         })
+        res.writeHead(200)
         res.end("true")
       }
     }
     else if (url_parts == "/userlist"){
+      res.writeHead(200)
       res.end(JSON.stringify(userList))
     }
     else if(url_parts == "/update"){      
@@ -72,9 +83,11 @@ const server = http.createServer((req, res) => {
       if (user){
         user.lastRequestTime = Date.now()
         if (user.mailBox.length > 0){
+          res.writeHead(200)
           res.end(JSON.stringify(user.mailBox))
         }
         else{
+          res.writeHead(404)
           res.end("clean")
         }
       }
@@ -83,7 +96,6 @@ const server = http.createServer((req, res) => {
       const url_query = url.parse(req.url, true).query
       const from = url_query['from']
       const to = getUser(url_query['to'])
-      console.log("get invite");
 
       if(to){
         to.mailBox.push({
@@ -91,9 +103,11 @@ const server = http.createServer((req, res) => {
           from: from,
           since: Date.now()
         })
+        res.writeHead(200)
         res.end("done")
       }
       else{
+        res.writeHead(404)
         res.end("User not found")
       }
     }
@@ -107,8 +121,9 @@ const server = http.createServer((req, res) => {
         rooms.push({
           id: id,
           p1: p1,
-          p1: p2,
-          turn: "red"
+          p2: p2,
+          turn: "red",
+          play: []
         })
   
         p1.mailBox = [{
@@ -122,9 +137,11 @@ const server = http.createServer((req, res) => {
           room: id
         }]
 
+        res.writeHead(200)
         res.end("done")
       }
       else{
+        res.writeHead(404)
         res.end("user not found")
       }
     }
@@ -136,12 +153,57 @@ const server = http.createServer((req, res) => {
           if (user.mailBox[i].type == "game")
           {
             user.mailBox.splice(i, 1)
+            res.writeHead(200)
             res.end("done")
             return
           }
         }
       }
+      res.writeHead(404)
       res.end("user not found")
+    }
+    else if (url_parts == "/updateGame"){      
+      const url_query = url.parse(req.url, true).query
+      const game = getGame(url_query['id'])
+
+      if (game){
+        res.writeHead(200)
+        res.end(JSON.stringify(game))
+      }
+      else{        
+        res.writeHead(404)
+        res.end("none")
+      }
+    }
+    else if (url_parts == "/newPlay"){
+      const url_query = url.parse(req.url, true).query
+      const game = getGame(url_query['id'])
+      const panel = url_query['panel']
+      const color = url_query['color']
+
+      console.log("new play", req.url);
+
+      if (game){
+        if (color == "red"){
+          game.turn = "blue"
+        }
+        else{
+          game.turn = "red"
+        }
+        console.log("game.turn", game.turn);
+
+        game.play.push({
+          color: color,
+          panel: panel
+        })
+        console.log(game);
+        res.writeHead(200)
+        res.end("done")
+      }
+      else{        
+        res.writeHead(404)
+        res.end("game not found")
+      }
     }
   }
 })
